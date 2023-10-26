fe51bf73eb31736605376faf2cfa567dde41164e
JoshuaRamc <joshuaramcharan818@gmail.com>
2023-10-25 21:40:42 -0400

    Add grid delete cells and add cells functionality

--- grid-maker/src/App.js
+++ grid-maker/src/App.js
@@ -1,27 +1,172 @@
 import "./App.css";
 import { Button } from "@mui/material";
 
+var rowCounter = 0;
+var columnCounter = 0;
+
+// Column Creator Function
+function columnCreator(currentRow, amountToAdd) {
+  let thisRow = document.getElementById("row" + currentRow);
+  for (let i = 2; i < amountToAdd + 2; i++) {
+    let newColumn = document.createElement("td");
+    newColumn.className = "box";
+    newColumn.id = "row" + currentRow + "-column" + i;
+    if (thisRow) {
+      thisRow.appendChild(newColumn);
+    }
+  }
+}
+
+function generateColumn() {
+  let grid = document.getElementById("dynamic-grid");
+  if (rowCounter === 0 && columnCounter === 0) {
+    rowCounter += 1;
+    columnCounter += 1;
+    let myRow = document.createElement("tr");
+    let rowData = document.createElement("td");
+    myRow.id = "row" + rowCounter;
+    rowData.className = "box";
+    rowData.id = "row" + rowCounter + "-column" + columnCounter;
+    if (grid) {
+      grid.appendChild(myRow);
+    }
+    if (myRow) {
+      myRow.appendChild(rowData);
+    }
+  } else {
+    for (let i = 1; i <= rowCounter; i++) {
+      let currentRow = document.getElementById("row" + i);
+      let myColumn = document.createElement("td");
+      var tempColumn = columnCounter + 1;
+      myColumn.id = "row" + i + "-column" + tempColumn;
+      myColumn.className = "box";
+      currentRow.appendChild(myColumn);
+    }
+    columnCounter += 1;
+  }
+  console.log("ColumnCount: ", columnCounter, "rowCount:", rowCounter);
+}
+
+function generateRow() {
+  let grid = document.getElementById("dynamic-grid");
+  if (columnCounter === 0 && rowCounter === 0) {
+    rowCounter += 1;
+    columnCounter += 1;
+    let myRow = document.createElement("tr");
+    let rowData = document.createElement("td");
+    myRow.id = "row" + rowCounter;
+    rowData.className = "box";
+    rowData.id = "row" + rowCounter + "-column" + columnCounter;
+    if (grid) {
+      grid.appendChild(myRow);
+    }
+    if (myRow) {
+      myRow.appendChild(rowData);
+    }
+  } else {
+    rowCounter += 1;
+    let myRow = document.createElement("tr");
+    let rowData = document.createElement("td");
+    myRow.id = "row" + rowCounter;
+    rowData.className = "box";
+    rowData.id = "row" + rowCounter + "-column1";
+    if (grid) {
+      grid.appendChild(myRow);
+    }
+    if (myRow) {
+      myRow.appendChild(rowData);
+    }
+    columnCreator(rowCounter, columnCounter - 1);
+  }
+  console.log("RowCount: ", rowCounter, "ColumnCount: ", columnCounter);
+}
+
+function deleteColumn() {
+  let grid = document.getElementById("dynamic-grid");
+  if (columnCounter > 0) {
+    for (let i = 0; i < rowCounter; i++) {
+      grid.rows[i].deleteCell(-1);
+    }
+    columnCounter--;
+
+    if (columnCounter === 0) {
+      for (let j = 0; j < rowCounter; j++) {
+        grid.deleteRow(-1);
+      }
+      rowCounter = 0;
+    }
+    console.log(
+      "Column Counter after remove: " +
+        columnCounter +
+        " rowCounter: " +
+        rowCounter
+    );
+  }
+}
+
+function deleteRow() {
+  let grid = document.getElementById("dynamic-grid");
+  if (rowCounter > 0) {
+    grid.deleteRow(-1);
+    rowCounter--;
+  }
+  if (rowCounter === 0) {
+    columnCounter = 0;
+  }
+  console.log(
+    "Column Counter after remove: " +
+      columnCounter +
+      " rowCounter: " +
+      rowCounter
+  );
+}
+
 function App() {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-      }}
-    >
-      <Button variant="contained" color="success" style={{ color: "white" }}>
-        Generate Column
-      </Button>
-      <Button variant="contained" color="success" style={{ color: "white" }}>
-        Generate Row
-      </Button>
-      <Button variant="contained" color="error" style={{ color: "white" }}>
-        Remove Column
-      </Button>
-      <Button variant="contained" color="error" style={{ color: "white" }}>
-        Remove Row
-      </Button>
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Button
+          variant="contained"
+          color="success"
+          style={{ color: "white" }}
+          id="generate-column"
+          onClick={(e) => generateColumn(e.target.value)}
+        >
+          Generate Column
+        </Button>
+        <Button
+          variant="contained"
+          color="success"
+          style={{ color: "white" }}
+          id="generate-row"
+          onClick={(e) => generateRow(e.target.value)}
+        >
+          Generate Row
+        </Button>
+        <Button
+          variant="contained"
+          color="error"
+          style={{ color: "white" }}
+          onClick={(e) => deleteColumn(e.target.value)}
+        >
+          Remove Column
+        </Button>
+        <Button
+          variant="contained"
+          color="error"
+          style={{ color: "white" }}
+          onClick={(e) => deleteRow(e.target.value)}
+        >
+          Remove Row
+        </Button>
+      </div>
+      <table bgcolor="white" id="dynamic-grid"></table>
     </div>
   );
 }
